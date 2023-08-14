dbacdaa24aac254844bdc47a9535458406ff1fb4
Francesco Gazzetta <fgaz@fgaz.me>
2023-08-14 07:47:07 +0200

    🌅

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixos-hardware.url = "github:nixos/nixos-hardware";
+
+
+  outputs = { self, nixpkgs, nixos-hardware, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "riscv64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSupportedSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      packages = forAllSupportedSystems (system: rec {
+        default = sd-image;
+        sd-image = (import "${nixpkgs}/nixos" {
+          configuration =
+            { config, ... }: {
+              imports = [
+                "${nixos-hardware}/pine64/star64/sd-image.nix"
+              ];
+
+              system.stateVersion = "23.05";
+              networking.useDHCP = true;
+              services.openssh.enable = true;
+
+              users.users.nixos = {
+                isNormalUser = true;
+                extraGroups = [ "wheel" ];
+              };
+              security.sudo.wheelNeedsPassword = false;
+              users.users.nixos.initialPassword = "nixos";
+            };
+          inherit system;
+        }).config.system.build.sdImage;
+      });
+    };
+}
