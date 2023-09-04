23d3c9af38adb4765ebde07d95fc9711b111b798
Francesco Gazzetta <fgaz@fgaz.me>
2023-09-04 20:54:17 +0200

    Make it more flakey

--- flake.nix
+++ flake.nix
@@ -14,91 +14,22 @@
       ];
       forAllSupportedSystems = nixpkgs.lib.genAttrs supportedSystems;
     in rec {
-      nixosConfigurations.star64 =
-        { config, pkgs, modulesPath, ... }: {
-          imports = [
-            "${nixos-hardware}/pine64/star64/sd-image.nix"
-            # Set the nixos channel to the nixpkgs the image was built with,
-            # to minimize rebuilds.
-            "${modulesPath}/installer/cd-dvd/channel.nix"
-          ];
+      nixosModules.star64 = ./configuration.nix;
 
-          system.stateVersion = "23.05";
-
-          nixpkgs.hostPlatform = "riscv64-linux";
-
-          # Uncomment on the 8GB model
-          #hardware.deviceTree.overlays = [{
-          #  name = "8GB-patch";
-          #  dtsFile = "${nixos-hardware}/pine64/star64/star64-8GB.dts";
-          #}];
-
-          networking.useDHCP = true;
-          networking.wireless.enable = true;
-          networking.wireless.userControlled.enable = true;
-          services.openssh.enable = true;
-
-          users.users.nixos = {
-            isNormalUser = true;
-            extraGroups = [ "wheel" ];
-          };
-          security.sudo.wheelNeedsPassword = false;
-          users.users.nixos.initialPassword = "nixos";
-          environment.systemPackages = with pkgs; [
-            git
-            htop
-            tmux
-          ];
-
-          # Provide a bunch of build dependencies to minimize rebuilds.
-          # Alternatively, sdImage.storePaths will not tie the packages to the system, allowing GC.
-          system.extraDependencies = with pkgs; builtins.concatMap (x: x.all) [
-            autoconf
-            automake
-            bash
-            binutils
-            bison
-            busybox
-            cargo
-            clang
-            cmake
-            config.boot.kernelPackages.kernel
-            curl
-            dtc
-            elfutils
-            flex
-            gcc
-            glibc
-            glibcLocales
-            jq
-            llvm
-            meson
-            ninja
-            pkg-config
-            python3
-            rustc
-            stdenv
-            stdenv.cc
-            stdenvNoCC
-            unzip
-            util-linux
-            zip
-            zlib
-          ];
-        };
+      nixosConfigurations.star64 = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit nixos-hardware; };
+        modules = [ nixosModules.star64 ];
+      };
 
       packages = forAllSupportedSystems (system: rec {
         default = sd-image;
-        sd-image = (import "${nixpkgs}/nixos" {
-          system = null; # Modular system/platform
-          configuration = nixosConfigurations.star64;
-        }).config.system.build.sdImage;
-        sd-image-cross = (import "${nixpkgs}/nixos" {
-          system = null; # Modular system/platform
-          configuration = {
-            nixpkgs.buildPlatform = system;
-            imports = [ nixosConfigurations.star64 ];
-          };
+        sd-image = nixosConfigurations.star64.config.system.build.sdImage;
+        sd-image-cross = (nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit nixos-hardware; };
+          modules = [
+            nixosModules.star64
+            { nixpkgs.buildPlatform = system; }
+          ];
         }).config.system.build.sdImage;
       });
     };
