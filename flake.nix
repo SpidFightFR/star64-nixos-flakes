b8ae5924bf140499876c07255ff6406120f22d5f
Francesco Gazzetta <fgaz@fgaz.me>
2023-09-04 20:59:50 +0200

    Add 8GB variants

--- flake.nix
+++ flake.nix
@@ -15,20 +15,39 @@
       forAllSupportedSystems = nixpkgs.lib.genAttrs supportedSystems;
     in rec {
       nixosModules.star64 = ./configuration.nix;
+      nixosModules."8gb-patch" = {
+        hardware.deviceTree.overlays = [{
+          name = "8GB-patch";
+          dtsFile = "${nixos-hardware}/pine64/star64/star64-8GB.dts";
+        }];
+      };
 
       nixosConfigurations.star64 = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit nixos-hardware; };
         modules = [ nixosModules.star64 ];
       };
+      nixosConfigurations.star64-8gb = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit nixos-hardware; };
+        modules = [ nixosModules.star64 nixosModules."8gb-patch" ];
+      };
 
       packages = forAllSupportedSystems (system: rec {
         default = sd-image;
         sd-image = nixosConfigurations.star64.config.system.build.sdImage;
+        sd-image-8gb = nixosConfigurations.star64-8gb.config.system.build.sdImage;
         sd-image-cross = (nixpkgs.lib.nixosSystem {
           specialArgs = { inherit nixos-hardware; };
           modules = [
             nixosModules.star64
             { nixpkgs.buildPlatform = system; }
+          ];
+        }).config.system.build.sdImage;
+        sd-image-cross-8gb = (nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit nixos-hardware; };
+          modules = [
+            nixosModules.star64
+            { nixpkgs.buildPlatform = system; }
+            nixosModules."8gb-patch"
           ];
         }).config.system.build.sdImage;
       });
